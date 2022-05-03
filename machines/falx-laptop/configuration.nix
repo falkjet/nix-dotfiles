5468e262472d296dc370136608407cd93e1390c6
Falk Markus Dursun Jetlund <falk@jetlund.com>
2022-05-03 20:50:56 +0200

    Enabled podman

--- machines/falx-laptop/configuration.nix
+++ machines/falx-laptop/configuration.nix
@@ -81,6 +81,7 @@
   ];
 
   virtualisation.docker.enable = true;
+  virtualisation.podman.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
