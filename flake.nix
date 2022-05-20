af85931995148ba8dab4829d139bd69518d6e6bf
Falk Markus Dursun Jetlund <falk@jetlund.com>
2022-05-20 19:03:18 +0200

    Updated dependencies and passing pkgs to home and nixos-config

--- flake.nix
+++ flake.nix
@@ -1,14 +1,27 @@
 {
   description = "A very basic flake";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
     home-manager.url = "github:nix-community/home-manager";
     wrappers.url = "github:falkjet/nix-wrapper-packages";
   };
-  outputs = { self, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, wrappers }:
+  let
+    system = "x86_64-linux";
+    pkgs = (import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    });
+    pkgs-unstable = (import nixpkgs-unstable {
+      inherit system;
+      config.allowUnfree = true;
+    });
+  in {
     homeConfigurations = {
-      falk = inputs.home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-linux";
+      falk = home-manager.lib.homeManagerConfiguration {
+        inherit system;
+        pkgs = pkgs-unstable;
         homeDirectory = "/home/falk";
         username = "falk";
         stateVersion = "22.05";
@@ -17,12 +30,12 @@
           imports = [ ./home/home.nix ];
           programs.git.userEmail = "falk@jetlund.com";
           programs.git.userName = "Falk Markus Dursun Jetlund";
-          home.packages = [ inputs.wrappers.packages.x86_64-linux.neovim ];
+          home.packages = [ wrappers.packages.x86_64-linux.neovim ];
         };
       };
     };
-    nixosConfigurations.falx-laptop = inputs.nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+    nixosConfigurations.falx-laptop = nixpkgs.lib.nixosSystem {
+      inherit system pkgs;
       modules =  [
         ({ pkgs, ... }: {
           imports = [ ./machines/falx-laptop/configuration.nix ];
